e82bb78a66528293e66faaf475342c51793f31c0
sash04ek <a.ovcharov@afterlogic.com>
2022-10-03 16:48:07 +0300

    Remove outdated EAV-related code - #30cgvqz

--- Module.php
+++ Module.php
@@ -27,18 +27,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	 * @ignore
 	 */
 	public function init()
-	{
-		\Aurora\Modules\Core\Classes\User::extend(
-			self::GetName(),
-			[
-				'Username'	=> ['string', ''],
-				'Email'	=> ['string', ''],
-				'Token'	=> ['string', ''],
-				'IdAccount'	=> ['int', 0],
-				'Suspended' => ['bool', false]
-			]
-		);
-		
+	{		
 		$this->subscribeEvent('CpanelIntegrator::DeleteAliases::after', array($this, 'onAfterDeleteAliases'));
 	}
 
