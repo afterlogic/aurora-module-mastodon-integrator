97b62d63dc1aca99b99983a75012f3553a7b5fda
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:58 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -20,280 +20,250 @@ use Aurora\Modules\Core\Models\User;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	/***** private functions *****/
-	/**
-	 * Initializes Mastodon Module.
-	 *
-	 * @ignore
-	 */
-	public function init()
-	{		
-		$this->subscribeEvent('CpanelIntegrator::DeleteAliases::after', array($this, 'onAfterDeleteAliases'));
-	}
+    /***** private functions *****/
+    /**
+     * Initializes Mastodon Module.
+     *
+     * @ignore
+     */
+    public function init()
+    {
+        $this->subscribeEvent('CpanelIntegrator::DeleteAliases::after', array($this, 'onAfterDeleteAliases'));
+    }
 
-	protected function getClient()
-	{
-		$oAuth = new \Colorfield\Mastodon\MastodonOAuth(
-			$this->getConfig('AppName'),
-			$this->getConfig('AppInstance')
-		);
-		$oAuth->config->setClientId($this->getConfig('ClientKey'));
-		$oAuth->config->setClientSecret($this->getConfig('ClientSecret'));
-		$oAuth->config->setBearer($this->getConfig('AccessToken'));
+    protected function getClient()
+    {
+        $oAuth = new \Colorfield\Mastodon\MastodonOAuth(
+            $this->getConfig('AppName'),
+            $this->getConfig('AppInstance')
+        );
+        $oAuth->config->setClientId($this->getConfig('ClientKey'));
+        $oAuth->config->setClientSecret($this->getConfig('ClientSecret'));
+        $oAuth->config->setBearer($this->getConfig('AccessToken'));
 
-		return new \Colorfield\Mastodon\MastodonAPI($oAuth->config);
-	}
+        return new \Colorfield\Mastodon\MastodonAPI($oAuth->config);
+    }
 
-	protected function getUserWithMastodonAccount($sEmail)
-	{
-		return User::firstWhere('Properties->'.$this->GetName() . '::Email', $sEmail);
-	}
+    protected function getUserWithMastodonAccount($sEmail)
+    {
+        return User::firstWhere('Properties->'.$this->GetName() . '::Email', $sEmail);
+    }
 
-	protected function suspendMastodonAccountByUser($oUser)
-	{
-		$bResult = false;
-		if (isset($oUser->{self::GetName().'::IdAccount'}))
-		{
-			$oClient = $this->getClient();
-			$aResult = $oClient->getResponse('/admin/accounts/' . $oUser->{self::GetName().'::IdAccount'} . '/action', 'post', [
-				'type' => 'suspend'
-			]);
+    protected function suspendMastodonAccountByUser($oUser)
+    {
+        $bResult = false;
+        if (isset($oUser->{self::GetName().'::IdAccount'})) {
+            $oClient = $this->getClient();
+            $aResult = $oClient->getResponse('/admin/accounts/' . $oUser->{self::GetName().'::IdAccount'} . '/action', 'post', [
+                'type' => 'suspend'
+            ]);
 
-			if (isset($aResult['error']))
-			{
-				throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
-			}
-			else
-			{
-				$oUser->setExtendedProp(self::GetName().'::Suspended', true);
-				$oUser->save();
-				$bResult = true;
-			}
-		}
-		return $bResult;
-	}
+            if (isset($aResult['error'])) {
+                throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
+            } else {
+                $oUser->setExtendedProp(self::GetName().'::Suspended', true);
+                $oUser->save();
+                $bResult = true;
+            }
+        }
+        return $bResult;
+    }
 
-	protected function unsuspendMastodonAccountByUser($oUser)
-	{
-		$bResult = false;
-		if (isset($oUser->{self::GetName().'::IdAccount'}))
-		{
-			$oClient = $this->getClient();
-			$aResult = $oClient->getResponse('/admin/accounts/' . $oUser->{self::GetName().'::IdAccount'} . '/unsuspend', 'post', []);
+    protected function unsuspendMastodonAccountByUser($oUser)
+    {
+        $bResult = false;
+        if (isset($oUser->{self::GetName().'::IdAccount'})) {
+            $oClient = $this->getClient();
+            $aResult = $oClient->getResponse('/admin/accounts/' . $oUser->{self::GetName().'::IdAccount'} . '/unsuspend', 'post', []);
 
-			if (isset($aResult['error']))
-			{
-				throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
-			}
-			else
-			{
-				$oUser->setExtendedProp(self::GetName().'::Suspended', false);
-				$oUser->save();
-				$bResult = true;
-			}
-		}
-		return $bResult;
-	}
+            if (isset($aResult['error'])) {
+                throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
+            } else {
+                $oUser->setExtendedProp(self::GetName().'::Suspended', false);
+                $oUser->save();
+                $bResult = true;
+            }
+        }
+        return $bResult;
+    }
 
-	public function onAfterDeleteAliases($aArgs, $mResult)
-	{
-		if (isset($aArgs['Aliases']))
-		{
-			foreach ($aArgs['Aliases'] as $sAlias)
-			{
-				$oUser = $this->getUserWithMastodonAccount($sAlias);
-			}
-		}
+    public function onAfterDeleteAliases($aArgs, $mResult)
+    {
+        if (isset($aArgs['Aliases'])) {
+            foreach ($aArgs['Aliases'] as $sAlias) {
+                $oUser = $this->getUserWithMastodonAccount($sAlias);
+            }
+        }
+    }
 
-	}
+    /***** private functions *****/
 
-	/***** private functions *****/
+    /***** public functions might be called with web API *****/
+    /**
+     * Obtains list of module settings for authenticated user.
+     *
+     * @return array
+     */
+    public function GetSettings()
+    {
+        $aResult = array();
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
 
-	/***** public functions might be called with web API *****/
-	/**
-	 * Obtains list of module settings for authenticated user.
-	 *
-	 * @return array
-	 */
-	public function GetSettings()
-	{
-		$aResult = array();
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
+        $oUser = \Aurora\System\Api::getAuthenticatedUser();
+        if (!empty($oUser) && $oUser->Role === \Aurora\System\Enums\UserRole::SuperAdmin) {
+            $aResult = [
+                'ClientKey' => $this->getConfig('ClientKey', ''),
+                'ClientSecret' => $this->getConfig('ClientSecret', ''),
+                'AccessToken' => $this->getConfig('AccesToken', '')
+            ];
+        }
 
-		$oUser = \Aurora\System\Api::getAuthenticatedUser();
-		if (!empty($oUser) && $oUser->Role === \Aurora\System\Enums\UserRole::SuperAdmin)
-		{
-			$aResult = [
-				'ClientKey' => $this->getConfig('ClientKey', ''),
-				'ClientSecret' => $this->getConfig('ClientSecret', ''),
-				'AccessToken' => $this->getConfig('AccesToken', '')
-			];
-		}
+        if (!empty($oUser) && $oUser->isNormalOrTenant()) {
+            if (!empty($oUser->{self::GetName().'::Token'})) {
+                $aResult = [
+                    'AccountUsername' => $oUser->{self::GetName().'::Username'},
+                    'AccountEmail' => $oUser->{self::GetName().'::Email'},
+                    'AccountSuspended' => $oUser->{self::GetName().'::Suspended'},
+                    'AccountConfirmed' => $this->IsMastodonAccountConfirmed()
+                ];
+            }
+        }
 
-		if (!empty($oUser) && $oUser->isNormalOrTenant())
-		{
-			if (!empty($oUser->{self::GetName().'::Token'}))
-			{
-				$aResult = [
-					'AccountUsername' => $oUser->{self::GetName().'::Username'},
-					'AccountEmail' => $oUser->{self::GetName().'::Email'},
-					'AccountSuspended' => $oUser->{self::GetName().'::Suspended'},
-					'AccountConfirmed' => $this->IsMastodonAccountConfirmed()
-				];
-			}
-		}
+        return $aResult;
+    }
 
-		return $aResult;
-	}
+    /**
+     * Updates settings.
+     *
+     * @throws \Aurora\System\Exceptions\ApiException
+     */
+    public function UpdateSettings($ClientKey, $ClientSecret, $AccesToken)
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::TenantAdmin);
 
-	/**
-	 * Updates settings.
-	 *
-	 * @throws \Aurora\System\Exceptions\ApiException
-	 */
-	public function UpdateSettings($ClientKey, $ClientSecret, $AccesToken)
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::TenantAdmin);
+        try {
+            $this->setConfig('ClientKey', $ClientKey);
+            $this->setConfig('ClientSecret', $ClientSecret);
+            $this->setConfig('AccesToken', $AccesToken);
+            $this->saveModuleConfig();
+        } catch (\Exception $ex) {
+            throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::CanNotSaveSettings);
+        }
 
-		try
-		{
-			$this->setConfig('ClientKey', $ClientKey);
-			$this->setConfig('ClientSecret', $ClientSecret);
-			$this->setConfig('AccesToken', $AccesToken);
-			$this->saveModuleConfig();
-		}
-		catch (\Exception $ex)
-		{
-			throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::CanNotSaveSettings);
-		}
+        return true;
+    }
 
-		return true;
-	}
+    public function RegisterMastodonAccount($Username, $Email, $Password, $Agreement = true, $Locale = 'en')
+    {
+        $mResult = false;
+        $oClient = $this->getClient();
+        $aResult = $oClient->getResponse('/accounts', 'post', [
+            'username' => $Username,
+            'email' => $Email,
+            'password' => $Password,
+            'agreement' => $Agreement,
+            'locale' => $Locale
+        ]);
 
-	public function RegisterMastodonAccount($Username, $Email, $Password, $Agreement = true, $Locale = 'en')
-	{
-		$mResult = false;
-		$oClient = $this->getClient();
-		$aResult = $oClient->getResponse('/accounts', 'post', [
-			'username' => $Username,
-			'email' => $Email,
-			'password' => $Password,
-			'agreement' => $Agreement,
-			'locale' => $Locale
-		]);
+        if (isset($aResult['error'])) {
+            throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
+        } else {
+            $aAccountsInfo = $oClient->getResponse(
+                '/admin/accounts',
+                'get',
+                [
+                    'username' => $Username,
+                    'email' => $Email
+                ]
+            );
+            if (isset($aAccountsInfo[0])) {
+                $oUser = \Aurora\System\Api::getAuthenticatedUser();
+                $oUser->setExtendedProp(self::GetName().'::Username', $Username);
+                $oUser->setExtendedProp(self::GetName().'::Email', $Email);
+                $oUser->setExtendedProp(self::GetName().'::Token', $aResult['access_token']);
+                $oUser->setExtendedProp(self::GetName().'::IdAccount', (int) $aAccountsInfo[0]['id']);
+                $oUser->save();
 
-		if (isset($aResult['error']))
-		{
-			throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
-		}
-		else
-		{
-			$aAccountsInfo = $oClient->getResponse(
-				'/admin/accounts',
-				'get',
-				[
-					'username' => $Username,
-					'email' => $Email
-				]
-			);
-			if (isset($aAccountsInfo[0]))
-			{
-				$oUser = \Aurora\System\Api::getAuthenticatedUser();
-				$oUser->setExtendedProp(self::GetName().'::Username', $Username);
-				$oUser->setExtendedProp(self::GetName().'::Email', $Email);
-				$oUser->setExtendedProp(self::GetName().'::Token', $aResult['access_token']);
-				$oUser->setExtendedProp(self::GetName().'::IdAccount', (int) $aAccountsInfo[0]['id']);
-				$oUser->save();
+                $mResult = true;
+            }
+        }
 
-				$mResult = true;
-			}
-		}
+        return $mResult;
+    }
 
-		return $mResult;
-	}
+    public function SuspendMastodonAccount()
+    {
+        return $this->suspendMastodonAccountByUser(
+            \Aurora\System\Api::getAuthenticatedUser()
+        );
+    }
 
-	public function SuspendMastodonAccount()
-	{
-		return $this->suspendMastodonAccountByUser(
-			\Aurora\System\Api::getAuthenticatedUser()
-		);
-	}
+    public function UnsuspendMastodonAccount()
+    {
+        return $this->unsuspendMastodonAccountByUser(
+            \Aurora\System\Api::getAuthenticatedUser()
+        );
+    }
 
-	public function UnsuspendMastodonAccount()
-	{
-		return $this->unsuspendMastodonAccountByUser(
-			\Aurora\System\Api::getAuthenticatedUser()
-		);
-	}
+    public function IsMastodonAccountConfirmed()
+    {
+        $mResult = false;
+        $oClient = $this->getClient();
+        $oUser = \Aurora\System\Api::getAuthenticatedUser();
+        $aResult = $oClient->getResponse('/admin/accounts/' . $oUser->{self::GetName().'::IdAccount'}, 'get', []);
 
-	public function IsMastodonAccountConfirmed()
-	{
-		$mResult = false;
-		$oClient = $this->getClient();
-		$oUser = \Aurora\System\Api::getAuthenticatedUser();
-		$aResult = $oClient->getResponse('/admin/accounts/' . $oUser->{self::GetName().'::IdAccount'}, 'get', []);
+        if (isset($aResult['error'])) {
+            throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
+        } else {
+            $mResult = isset($aResult['confirmed']) ? $aResult['confirmed'] : false;
+        }
 
-		if (isset($aResult['error']))
-		{
-			throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
-		}
-		else
-		{
-			$mResult = isset($aResult['confirmed']) ? $aResult['confirmed'] : false;
-		}
+        return $mResult;
+    }
 
-		return $mResult;
-	}
+    public function LoginToMastodonAccount($Username, $Password)
+    {
+        $oAuth = new \Colorfield\Mastodon\MastodonOAuth(
+            $this->getConfig('AppName'),
+            $this->getConfig('AppInstance')
+        );
+        $oAuth->config->setClientId($this->getConfig('ClientKey'));
+        $oAuth->config->setClientSecret($this->getConfig('ClientSecret'));
+        $oAuth->config->setBearer($this->getConfig('AccessToken'));
+        $oAuth->config->setScopes(['read', 'write', 'follow']);
 
-	public function LoginToMastodonAccount($Username, $Password)
-	{
-		$oAuth = new \Colorfield\Mastodon\MastodonOAuth(
-			$this->getConfig('AppName'),
-			$this->getConfig('AppInstance')
-		);
-		$oAuth->config->setClientId($this->getConfig('ClientKey'));
-		$oAuth->config->setClientSecret($this->getConfig('ClientSecret'));
-		$oAuth->config->setBearer($this->getConfig('AccessToken'));
-		$oAuth->config->setScopes(['read', 'write', 'follow']);
+        return $oAuth->getAuthorizationUrl();
+    }
 
-		return $oAuth->getAuthorizationUrl();
-	}
+    public function ChangeMastodonAccountPassword($NewPassword)
+    {
+        $oUser = \Aurora\System\Api::getAuthenticatedUser();
+        if (isset($oUser->{self::GetName().'::IdAccount'})) {
+            return $this->LoginToMastodonAccount($oUser->{self::GetName().'::Username'}, $NewPassword);
+        }
+    }
 
-	public function ChangeMastodonAccountPassword($NewPassword)
-	{
-		$oUser = \Aurora\System\Api::getAuthenticatedUser();
-		if (isset($oUser->{self::GetName().'::IdAccount'}))
-		{
-			return $this->LoginToMastodonAccount($oUser->{self::GetName().'::Username'}, $NewPassword);
-		}
-	}
+    public function SendPostMessage($Message, $Direct = false)
+    {
+        $mResult = false;
+        $oUser = \Aurora\System\Api::getAuthenticatedUser();
+        if (isset($oUser->{self::GetName().'::IdAccount'})) {
+            $oClient = $this->getClient();
+            $options = [
+                'status' => $Message,
+            ];
+            if ($Direct) {
+                $options['visibility'] = 'direct';
+            }
+            $aResult = $oClient->getResponse('/statuses', 'post', $options, $oUser->{self::GetName().'::Token'});
+            if (isset($aResult['error'])) {
+                throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
+            } else {
+                $mResult = true;
+            }
+        }
 
-	public function SendPostMessage($Message, $Direct = false)
-	{
-		$mResult = false;
-		$oUser = \Aurora\System\Api::getAuthenticatedUser();
-		if (isset($oUser->{self::GetName().'::IdAccount'}))
-		{
-			$oClient = $this->getClient();
-			$options = [
-				'status' => $Message,
-			];
-			if ($Direct)
-			{
-				$options['visibility'] = 'direct';
-			}
-			$aResult = $oClient->getResponse('/statuses', 'post', $options, $oUser->{self::GetName().'::Token'});
-			if (isset($aResult['error']))
-			{
-				throw new \Aurora\System\Exceptions\ApiException(0, null, $aResult['error']);
-			}
-			else
-			{
-				$mResult = true;
-			}
-		}
-
-		return $mResult;
-	}
-	/***** public functions might be called with web API *****/
+        return $mResult;
+    }
+    /***** public functions might be called with web API *****/
 }
